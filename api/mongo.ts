b43a0415bf042bb28316b053ed68059d9be8189d
tntan04 <tntankg@gmail.com>
2026-02-11 21:42:07 +0700

    Update mongo.ts

--- api/mongo.ts
+++ api/mongo.ts
@@ -1,22 +1,21 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb"
 
-const uri = process.env.MONGODB_URI as string;
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
+const uri = process.env.MONGODB_URI as string
 
 if (!uri) {
-  throw new Error("Please add MONGODB_URI to Vercel Environment Variables");
+  throw new Error("Please add MONGODB_URI to Environment Variables")
 }
+
+const options = {}
+
+let client
+let clientPromise: Promise<MongoClient>
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  global._mongoClientPromise = client.connect();
+  client = new MongoClient(uri, options)
+  global._mongoClientPromise = client.connect()
 }
 
-clientPromise = global._mongoClientPromise;
+clientPromise = global._mongoClientPromise
 
-export async function getDB() {
-  const client = await clientPromise;
-  return client.db();
-}
+export default clientPromise
